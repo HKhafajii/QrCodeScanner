55a9dcddc22942c133b23ae5b764dad2edacf4ce
HKhafajii <147748541+HKhafajii@users.noreply.github.com>
2025-01-16 13:36:28 -0500

    l

--- HWSQRCode/ContentView.swift
+++ HWSQRCode/ContentView.swift
@@ -6,17 +6,57 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+    enum FilterType {
+        case none, contacted, uncontacted
     }
+    
+    let filter: FilterType = .none
+    @State private var isShowingScanner = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+                Text("Prospect Name")
+                
+                Text("Prospect address")
+                
+                
+                
+                
+            } // End of VStack
+            .padding()
+            .navigationTitle("Everyone")
+            .toolbar {
+                Button("Scan", systemImage: "qrcode.viewfinder") {
+                    isShowingScanner = true
+                }
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Hassan alkhafaji\nalkhafajihassan@gmail.com", completion: handleScan)
+            }
+        }
+    }
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        
+        isShowingScanner = false
+        
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+//            Make new person clas of information from code
+//            Insert new person to the model
+            
+        case .failure(let error):
+            print("scanning failed: \(error.localizedDescription)")
+        }    }
 }
 
 #Preview {
